f1504180421b0d0c92998ac313a6b0bda9f5d4dc
James Norton <52245522+swairmi@users.noreply.github.com>
2019-07-17 11:15:12 +0100

    Add files via upload

---
+++ zelogo.js
@@ -0,0 +1,15 @@
+/*
+const ani1 = document.getElementsByClassName('waveybanner');
+document.addEventListener('click', function (event) {
+
+var ease = anime({
+  targets: '.waveybanner , .st5, .st3',
+    translateY: [
+     { value: -50, duration: 1000, delay: 10},
+     { value: 150, duration: 1000, delay: 10},
+   ]
+
+});
+
+});
+*/
